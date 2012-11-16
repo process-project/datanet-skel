7d420f81c6ba8230b434c6c9876491fda2950e4a
Marek Kasztelnik <mkasztelnik@gmail.com>
2012-11-16 13:04:33 +0100

    Initial commit

---
+++ lib/datanet-skel.rb
@@ -0,0 +1,7 @@
+require "datanet-skel/version"
+
+module Datanet
+  module Skel
+    # Your code goes here...
+  end
+end
---
+++ lib/datanet-skel/version.rb
@@ -0,0 +1,5 @@
+module Datanet
+  module Skel
+    VERSION = "0.0.1"
+  end
+end
